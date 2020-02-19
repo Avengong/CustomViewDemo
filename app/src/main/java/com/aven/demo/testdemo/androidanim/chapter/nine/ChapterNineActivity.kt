27d17abec9a40452ff1399019204e6e2271282b5
gongwen <635583021@qq.com>
2020-02-19 22:49:57 +0800

    [Modify]  第九章 画布（bitmap）、图层、canvas的关系。

---
+++ app/src/main/java/com/aven/demo/testdemo/androidanim/chapter/nine/ChapterNineActivity.kt
@@ -0,0 +1,15 @@
+package com.aven.demo.testdemo.androidanim.chapter.nine
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.aven.demo.testdemo.R
+
+class ChapterNineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter_nine)
+
+
+    }
+}
